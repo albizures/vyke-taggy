af67d582edeca92d065d74f10f5274d66db6aaeb
Jose Albizures <jose@albizures.com>
2025-02-13 12:16:41 -0600

    feat: create effect once - improve types - export TagChild

--- src/conditional.ts
+++ src/conditional.ts
@@ -1,29 +1,25 @@
+import type { ReadSignal, Signal } from './signals'
 import type { TagChild } from './tag-handler'
 import { computed } from 'alien-signals'
-
-type Getter<T> = {
-	(): T
-	(value: T): void
-}
 
 type Case<TValue> = [value: TValue, () => TagChild]
 
 export class Conditional<TValue> {
 	constructor(
-		readonly getter: Getter<TValue>,
+		readonly signal: ReadSignal<TValue>,
 		readonly cases: Array<Case<TValue>>) {}
 }
 
 /**
  * Render the given element based on given cases
  */
-export function when<TValue>(getter: Getter<TValue>, ...cases: Array<Case<TValue>>): Conditional<TValue> {
-	return new Conditional(getter, cases)
+export function when<TValue>(signal: ReadSignal<TValue> | Signal<TValue>, ...cases: Array<Case<TValue>>): Conditional<TValue> {
+	return new Conditional(signal, cases)
 }
 
 export function match<TValue>(conditional: Conditional<TValue>) {
 	return computed(() => {
-		const value = conditional.getter()
+		const value = conditional.signal()
 		for (const [caseValue, handler] of conditional.cases) {
 			if (value === caseValue) {
 				return handler()
--- src/signals/index.ts
+++ src/signals/index.ts
@@ -1,5 +1,14 @@
+import { effect } from 'alien-signals'
+
 export * from './signal'
 export * from './syncValue'
 export * from './toggle'
 
 export { computed, effect, signal } from 'alien-signals'
+
+export function effectOnce(fn: () => void) {
+	const stop = effect(() => {
+		fn()
+		stop()
+	})
+}
